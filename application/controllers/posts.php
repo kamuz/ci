1cc5a7589f02e95d7c612094bfcf2b2d54d7701b
Vladimir Kamuz <v.kamuz@gmail.com>
2016-11-03 22:58:53 +0200

    Output data from database

--- application/controllers/posts.php
+++ application/controllers/posts.php
@@ -1,11 +1,13 @@
 <?php
 
 class Posts extends CI_Controller{
+
     function index(){
         $this->load->model('post');
-        $data['posts'] = $this->post->get_posts();
+        $data = $this->post->get_posts();
         echo '<pre>';
-        print_r($data['posts']);
+        print_r($data);
         echo '</pre>';
     }
+
 }
--- application/models/post.php
+++ application/models/post.php
@@ -1,13 +1,11 @@
 <?php
 
 class Post extends CI_Model{
-   
-   function get_posts($num = 20, $start = 0){
-       // $sql = "SELECT * FROM posts";
-       $this->db->select()->from('posts')->where('active', 1)->order_by('date_added', 'desc')->limit(0, 20);
-       // $query = mysql_query($sql);
-       $query = $this->db->get();
-       return $query->result_array();
+
+   function  get_posts(){
+       $this->db->select()->from('posts')->order_by('date_added', 'desc')->where('active', 1);
+       $sql = $this->db->get();
+       return $query = $sql->result_array();
    }
-    
+
 }
