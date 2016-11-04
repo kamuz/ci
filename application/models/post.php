ef471634b82066ed1b6fadcd4261ad14b66a3e87
Vladimir Kamuz <v.kamuz@gmail.com>
2016-11-04 06:55:18 +0200

    Another syntax to prepare SQL statement

--- application/models/post.php
+++ application/models/post.php
@@ -2,8 +2,13 @@
 
 class Post extends CI_Model{
 
- function  get_posts(){
-    $this->db->select()->from('posts')->order_by('date_added', 'desc')->where('active', 1);
+ function  get_posts($num = 20, $start = 0){
+    $this->db->select('*');
+    $this->db->from('posts');
+    $this->db->where('active', 1);
+    $this->db->where(array('active >=' => 1));
+    $this->db->order_by('date_added', 'desc');
+    $this->db->limit($num, $start);
     $sql = $this->db->get();
     return $query = $sql->result_array();
   }
