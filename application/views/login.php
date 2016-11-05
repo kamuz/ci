d15aa0d0df64e475fc64a6b06f53443d1e0ca4b8
Vladimir Kamuz <v.kamuz@gmail.com>
2016-11-05 15:29:32 +0200

    Added list of select for user type

--- application/views/login.php
+++ application/views/login.php
@@ -13,6 +13,14 @@
 <form action="<?php echo base_url()?>users/login" method="post">
   <p>Username: <input type="text" name="username"></p>
   <p>Password: <input type="password" name="password"></p>
+  <p>User Type:
+    <select name="user_type">
+      <option value="" selected="selected">--</option>
+      <option value="admin">Admin</option>
+      <option value="author">Author</option>
+      <option value="user">User</option>
+    </select>
+  </p>
   <p><input type="submit" value="Login"></p>
 </form>
 </body>
