d5ebc075797a55dfb4fdc98aaa1f504bf8e3e8bf
Vladimir Kamuz <v.kamuz@gmail.com>
2016-11-05 09:28:35 +0200

    Added model user

---
+++ application/models/user.php
@@ -0,0 +1,19 @@
+<?php
+
+class User extends CI_Model{
+
+  function create_user($data){
+    $this->db->insert('users', $data);
+  }
+
+  function login($username, $password){
+    $where = array(
+      'username' => $username,
+      'password' => $password
+    );
+    $this->db->select()->from('users')->where($where);
+    $query = $this->db->get();
+    return $query->first_row('array');
+  }
+
+}
