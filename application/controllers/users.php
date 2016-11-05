4d43d8114dd622a783609987dc5f9ed018ea45be
Vladimir Kamuz <v.kamuz@gmail.com>
2016-11-05 19:33:15 +0200

    Setting form

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -2,6 +2,23 @@
 
 class Users extends CI_Controller{
 
+  function register(){
+    if($_POST){
+      $data = array(
+        'username' => $_POST['username'],
+        'password' => ($_POST['password']),
+        'user_type' => $_POST['user_type']
+      );
+      $this->load->model('user');
+      $user_id = $this->user->create_user($data);
+      $this->session->set_userdata('user_id', $user_id);
+      $this->session->set_userdata('user_type', $_POST['user_type']);
+      redirect(base_url() . 'posts');
+    }
+    $this->load->helper('form');
+    $this->load->view('register_user');
+  } 
+
   function login(){
     $data['error'] = 0;
     if($_POST){
--- application/views/register_user.php
+++ application/views/register_user.php
@@ -6,5 +6,18 @@
 </head>
 <body>
   <h1>Registration</h1>
+  <?php echo form_open(base_url() . 'users/register'); ?>
+    <p>Username: <input type="text" name="username"></p>
+    <p>Password: <input type="password" name="password"></p>
+    <p>User Type:
+      <select name="user_type">
+        <option value="" selected="selected">--</option>
+        <option value="admin">Admin</option>
+        <option value="author">Author</option>
+        <option value="user">User</option>
+      </select>
+    </p>
+    <p><?php echo form_submit('', 'Register'); ?></p>
+  <?php echo form_close(); ?>
 </body>
 </html>
