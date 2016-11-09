05d77c38bfabb78c53feaf9058063f63154ba82e
Vladimir Kamuz <v.kamuz@gmail.com>
2016-11-09 10:33:02 +0200

    Added link to upload another file and print data like array

--- application/views/upload_success.php
+++ application/views/upload_success.php
@@ -1,6 +1,10 @@
 <h3 style="color: green">Your file was successfully uploaded!</h3>
-<ul>
-  <?php foreach($upload_data as $key => $value): ?>
-    <li><?php echo $key . ' : ' . $value ?></li>
-  <?php endforeach; ?>
-</ul>
+
+<?php
+echo '<pre>';
+print_r($upload_data);
+echo '</pre>';
+
+?>
+
+<a href="<?php base_url() ?>upload">Upload another file</a>
